78312a711f74dcaadd73f97324f7e1e677eecd42
Kaloyan Yosifov <kaloqn665@gmail.com>
2021-02-24 19:36:55 +0200

    Init truffle

--- contracts/Calculator.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Calculator {
-    function add(int  number, int number2) public pure returns (int) {
-        return number + number2;
-    }
-}
